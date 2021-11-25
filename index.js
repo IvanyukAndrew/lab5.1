43731974736d3f0cf7af603a7c5788c0e23495dd
IvanyukAndrew <73875988+IvanyukAndrew@users.noreply.github.com>
2021-11-25 15:37:49 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+let Tournament = [{
+    id: 0,
+    fullName: "Ivanyuk Andrii Grugoriyovich",
+    age: 18,
+    sex: "man",
+    country: "Ukraine",
+    markOne: 3,
+    markTwo: 5,
+    markThree: 4
+},
+{
+    id: 1,
+    fullName: "Vitto Scaleto Bargo",
+    age: 25,
+    sex: "man",
+    country: "Italy",
+    markOne: 5,
+    markTwo: 5,
+    markThree: 5
+},
+{
+    id: 2,
+    fullName: "Ya na Fabruzi",
+    age: 18,
+    sex: "woman",
+    country: "Ukraine",
+    markOne: 4,
+    markTwo: 5,
+    markThree: 4
+},
+];
+
+app.get("/tournament", (req, res) => {
+    res.send(Tournament);
+});
+
+app.get("/tournament/query", (req, res) => {
+    let queryTournament = Tournament;
+    if (req.query.minage)
+        queriedTournament = queriedTournament.filter(
+            (tournament) => tournament.age >= parseFloat(req.query.minage)
+        );
+    if (req.query.fullName)
+        queriedTournament = queriedTournament.filter(
+            (tournament) => tournament.fullName.includes(req.query.fullName)
+        );
+    res.send(queriedTournament);
+});
+
+app.get("/tournament/:id", (req, res) => {
+    let tournament = Tournament.find((tournament) => tournament.id === parseInt(req.params.id));
+    if (tournament !== null) res.status(200).send(book);
+    else res.status(404).send("Not Found");
+});
+
+app.delete("/tournament/:id", (req, res) => {
+    let index = Tournament.findIndex((tournament) => tournament.id === parseInt(req.params.id));
+    if (index >= 0) {
+        let deletedTournament = Tournament[index];
+        Tournament.splice(index, 1);
+        res.send(deletedTournament);
+    } else res.status(404).send("Not Found");
+});
+
+app.post("/tournament", (req, res) => {
+    let newTournament = {
+        id: Number(Date.now()),
+        ...req.body,
+    };
+    Tournament.push(newTournament);
+    res.send(newTournament);
+});
+
+app.all("/", (req, res) => {
+    res.send("Test");
+});
+
+app.listen(3000, () => {
+    console.log("http://localhost:3000");
+});
